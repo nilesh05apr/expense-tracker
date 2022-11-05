b2c4251d464d63d00697f35af2103a594abc257e
nilesh05apr <nk8588338@gmail.com>
2022-11-06 05:12:55 +0530

    add: MongoDB Connection

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+cf	"backend/pkg/configs" 
+)
+
+
+func main() {	
+	app := fiber.New()
+
+	cf.ConnectDB()
+	
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Hello, World!"})
+	})
+	app.Listen(":3000")
+}
--- backend/pkg/configs/setup.go
+++ backend/pkg/configs/setup.go
@@ -1,1 +1,48 @@
 package configs
+
+import ( 
+	"context"
+	"log"
+	"time"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+)
+
+
+
+func ConnectDB() *mongo.Client {
+
+	// Set client options
+	client, err := mongo.NewClient(options.Client().ApplyURI(LoadEnv()))
+
+	if err != nil {		
+		log.Fatal(err)
+		fmt.Println(LoadEnv())
+	}
+
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cancel()
+
+	// Check the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client
+}
+
+var DB *mongo.Client = ConnectDB()
+
+func GetCollection(collection string) *mongo.Collection {
+	return DB.Database("expenseDB").Collection(collection)
+}
