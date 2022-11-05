3aa6ce0a05894aa6b9cc8f13df0dbb0afbb3c1c1
nilesh05apr <nk8588338@gmail.com>
2022-11-06 05:26:53 +0530

    add: models

--- backend/pkg/models/models.go
+++ backend/pkg/models/models.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Expense struct {
+	ID 			primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Amount 		float64 		  	`json:"amount,omitempty" bson:"amount,omitempty"`
+	Description string 	  			`json:"description,omitempty" bson:"description,omitempty"`
+	Note 		string 		  		`json:"note,omitempty" bson:"note,omitempty"`
+	CreatedAt 	time.Time   		`json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+}
+
+type ExpenseCategory struct {
+	ID 			primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Name 		string 		  		`json:"name,omitempty" bson:"name,omitempty"`
+	Amount 		float64 		  	`json:"amount,omitempty" bson:"amount,omitempty"`
+	MaxAmount 	float64 	  		`json:"maxAmount,omitempty" bson:"maxAmount,omitempty"`
+	Expenses 	[]Expense 	  		`json:"expenses,omitempty" bson:"expenses,omitempty"`
+}
