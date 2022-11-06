15811636ed00f88fe423121879de13a64c9e23e1
nilesh05apr <nk8588338@gmail.com>
2022-11-06 09:04:55 +0530

    update: changed note to name

--- backend/pkg/models/models.go
+++ backend/pkg/models/models.go
@@ -9,7 +9,7 @@ type Expense struct {
 	ID 					primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
 	Amount 				float64 		  	`json:"amount,omitempty" bson:"amount,omitempty"`
 	Description 		string 	  			`json:"description,omitempty" bson:"description,omitempty"`
-	Name 				string 		  		`json:"note,omitempty" bson:"note,omitempty"`
+	Name 				string 		  		`json:"name,omitempty" bson:"name,omitempty"`
 	CreatedAt 			time.Time   		`json:"createdAt,omitempty" bson:"createdAt,omitempty"`
 	ExpenseCategoryID 	primitive.ObjectID 	`json:"expenseCategoryId,omitempty" bson:"expenseCategoryId,omitempty"`
 }
