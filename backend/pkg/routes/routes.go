b03c50b5f831edb5428a54038d23374f678e72de
nilesh05apr <nk8588338@gmail.com>
2022-11-06 06:06:45 +0530

    add: Router

--- backend/pkg/routes/routes.go
+++ backend/pkg/routes/routes.go
@@ -1,1 +1,20 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"backend/pkg/controllers"
+
+)
+
+func Routes(app *fiber.App) {
+	app.Get("/api/expenses", controllers.GetExpenses)
+	app.Get("/api/expenses/:id", controllers.GetExpense)
+	app.Post("/api/expenses", controllers.CreateExpense)
+	app.Put("/api/expenses/:id", controllers.UpdateExpense)
+	app.Delete("/api/expenses/:id", controllers.DeleteExpense)
+	app.Get("/api/expenseCategories", controllers.GetExpenseCategories)
+	app.Get("/api/expenseCategories/:id", controllers.GetExpenseCategory)
+	app.Post("/api/expenseCategories", controllers.CreateExpenseCategory)
+	app.Put("/api/expenseCategories/:id", controllers.UpdateExpenseCategory)
+	app.Delete("/api/expenseCategories/:id", controllers.DeleteExpenseCategory)
+}
