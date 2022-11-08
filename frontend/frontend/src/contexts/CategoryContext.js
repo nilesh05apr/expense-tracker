a55e902a374494a955529f3f40c5f3bed7e05b41
nilesh05apr <nk8588338@gmail.com>
2022-11-08 14:31:04 +0530

    add: contexts

---
+++ frontend/frontend/src/contexts/CategoryContext.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+const CategoryContext = React.createContext();
+
+export function useCategory() {
+  return useContext(CategoryContext);
+}
+
+export const CategoryProvider = ({ children }) => {
+  const [category, setCategory] = useLocalStorage('category', []);
+  const [expense, setExpense] = useLocalStorage('expense', []);
+
+    const createCategory = (name, maxAmount) => {
+        setCategory(prevCategory => {
+            return [...prevCategory, { name, maxAmount }]
+        })
+    }
+
+    const deleteCategory = (id) => {
+        setCategory(prevCategory => {
+            return prevCategory.filter(category => category.id !== id)
+        })
+    }
+
+    const createExpense = (name, amount, description, category) => {
+        setExpense(prevExpense => {
+            return [...prevExpense, { name, amount, description, category }]
+        })
+    }
+
+    const deleteExpense = (id) => {
+        setExpense(prevExpense => {
+            return prevExpense.filter(expense => expense.id !== id)
+        })
+    }
+
+    const value = {
+        category,
+        expense,
+        createCategory,
+        deleteCategory,
+        createExpense,
+        deleteExpense
+    }
+
+  return (
+    <CategoryContext.Provider value={value}>
+      {children}
+    </CategoryContext.Provider>
+  );
+}
