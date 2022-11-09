b65c15acf0a15b425b2b9bfce09993641067d067
nilesh05apr <nk8588338@gmail.com>
2022-11-09 15:29:27 +0530

    add: default amount

--- backend/pkg/models/models.go
+++ backend/pkg/models/models.go
@@ -17,6 +17,6 @@ type Expense struct {
 type ExpenseCategory struct {
 	ID 			primitive.ObjectID 		`json:"_id,omitempty" bson:"_id,omitempty"`
 	Name 		string 		  			`json:"name,omitempty" bson:"name,omitempty"`
-	Amount 		float64 		  		`json:"amount,omitempty" bson:"amount,omitempty"`
+	Amount 		float64 		  		`json:"amount,omitempty" bson:"amount,omitempty" default:"0"`
 	MaxAmount 	float64 	  			`json:"maxAmount,omitempty" bson:"maxAmount,omitempty"`
 }
